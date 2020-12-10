mattboehm/vim-unstack

commit 163cf1219dc7363d55ca1c29808406d0a9c6635e

Adjust Python/Ruby patterns to make trailing characters optional

autoload/unstack/extractors.vim
@@ -28,9 +28,9 @@ function! unstack#extractors#GetDefaults()
   "me cry
   let extractors = []
   "Python
-  call add(extractors, unstack#extractors#Regex('\v^ *File "([^"]+)", line ([0-9]+).+', '\1', '\2'))
+  call add(extractors, unstack#extractors#Regex('\v^ *File "([^"]+)", line ([0-9]+).?', '\1', '\2'))
   "Ruby
-  call add(extractors, unstack#extractors#Regex('\v^[ \t]*from (.+):([0-9]+):in `.+', '\1', '\2'))
+  call add(extractors, unstack#extractors#Regex('\v^[ \t]*from (.+):([0-9]+):in `.?', '\1', '\2'))
   "C#
   call add(extractors, unstack#extractors#Regex('\v^[ \t]*at .*\(.*\) in (.+):line ([0-9]+) *$', '\1', '\2'))
   "Perl

commit dfe1ebe906692868e4b52fa5069e24b047e2622e

Python/Ruby can more than one trailing character
This fixes a bug introduced by 163cf12, which made trailing characters optional, but restricted them (if present) to only a single one.

autoload/unstack/extractors.vim
@@ -28,9 +28,9 @@ function! unstack#extractors#GetDefaults()
   "me cry
   let extractors = []
   "Python
-  call add(extractors, unstack#extractors#Regex('\v^ *File "([^"]+)", line ([0-9]+).?', '\1', '\2'))
+  call add(extractors, unstack#extractors#Regex('\v^ *File "([^"]+)", line ([0-9]+).*', '\1', '\2'))
   "Ruby
-  call add(extractors, unstack#extractors#Regex('\v^[ \t]*from (.+):([0-9]+):in `.?', '\1', '\2'))
+  call add(extractors, unstack#extractors#Regex('\v^[ \t]*from (.+):([0-9]+):in `.*', '\1', '\2'))
   "C#
   call add(extractors, unstack#extractors#Regex('\v^[ \t]*at .*\(.*\) in (.+):line ([0-9]+) *$', '\1', '\2'))
   "Perl